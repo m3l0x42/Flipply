d14a38118236dd66ad36ecacde34ed284ba6447c
Mario Casas <a01287116@tec.mx>
2025-10-04 13:03:24 -0400

    Local changes before the sync

--- App/app/(tabs)/_layout.tsx
+++ App/app/(tabs)/_layout.tsx
@@ -1,10 +1,33 @@
 import { Stack } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+import { View, Text } from 'react-native'; // ✅ added imports
+
+function FlipplyHeaderTitle() {
+  return (
+    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <Ionicons name="mail" size={30} color="blue" />
+      <Text style={{ marginLeft: 5, fontSize: 25 }}>  Flipply</Text>
+    </View>
+  );
+}
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Image Upload' }} />
-      <Stack.Screen name="results" options={{ title: 'Results' }} />
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: () => <FlipplyHeaderTitle />,
+          headerStyle: {
+            backgroundColor: "grey" // color AppBar
+          },
+        }}
+      />
+
+      <Stack.Screen
+        name="results"
+        options={{ title: 'Results' }}
+      />
     </Stack>
   );
 }
